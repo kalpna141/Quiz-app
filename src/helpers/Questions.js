5e3d8486ec02d84a059213825c3cb88290cbf75d
kalpna141 <kalpnathakurr@gmail.com>
2023-07-18 15:08:44 +0530

    first commit

---
+++ src/helpers/Questions.js
@@ -0,0 +1,96 @@
+export const Questions=[
+    {
+prompt:"What is 1+3",
+optionA:'24',
+optionB:'21',
+optionC:'4',
+optionD:'2',
+answer:'optionC'
+
+    },
+    {
+        prompt:" which is a streaming platform?",
+        optionA:"Twitter",
+        optionB:"Twitch",
+        optionC:"Google",
+        optionD:"Robinhood",
+        answer:'optionB'
+
+    },
+    {
+        prompt:"Which of the following is used in React.js to increase the performance?",
+        optionA:"Original DOM",
+        optionB:"Virtual DOM",
+        optionC:"Both A and B",
+        optionD:"None of the above",
+        answer:'optionB'
+
+    },
+    {
+        prompt:"Which of the following act as the input of a class-based component?",
+        optionA:"Class",
+        optionB:"Render",
+        optionC:"Factory",
+        optionD:"Props",
+        answer:'optionD'
+
+    },
+    {
+        prompt:"How many numbers of elements a valid react component can return?",
+        optionA:"1",
+        optionB:"2",
+        optionC:"4",
+        optionD:"5",
+        answer:'optionA'
+
+    },
+    {
+        prompt:"What is the default port where webpack-server runs?",
+        optionA:"3000",
+        optionB:"8080",
+        optionC:"3030",
+        optionD:"6060",
+        answer:'optionB'
+
+    },
+    {
+        prompt:"How many ways of defining your variables in ES6?",
+        optionA:"1",
+        optionB:"3",
+        optionC:"4",
+        optionD:"5",
+        answer:'optionB'
+
+    },
+    {
+        prompt:"What are the two ways to handle data in React?",
+        optionA:"State and Props",
+        optionB:"Services & Components",
+        optionC:"State & Services",
+        optionD:"State & Component",
+        answer:'optionA'
+
+    },
+    {
+        prompt:"Which of the following is used to pass data to a component from outside in React.js?",
+        optionA:"SetState",
+        optionB:"Render with argumentns",
+        optionC:"Props",
+        optionD:"Prototypes",
+        answer:'optionC'
+
+    },
+    {
+        prompt:" What does ES6 stand for?",
+        optionA:"ECMAScript 6",
+        optionB:"ECMA 6",
+        optionC:"ECMAJavaScript 6",
+        optionD:"EJavaScript 6",
+        answer:'optionA'
+
+    },
+
+
+
+
+];
